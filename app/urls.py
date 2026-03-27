e791f0217dbd1909338e08339bcd9ddeee1326b4
salva <ascolong@gmail.com>
2026-03-27 14:35:12 -0600

    Se creo el apartado para proveedores

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import auth, dashboard, productos, categorias, clientes
+from .views import auth, dashboard, productos, categorias, clientes, proveedores
 
 urlpatterns = [
     path("", auth.login_view, name="login"),
@@ -24,4 +24,10 @@ urlpatterns = [
     path("clientes/nuevo/", clientes.crear_cliente, name="crear_cliente"),
     path("clientes/editar/<int:id>/", clientes.editar_cliente, name="editar_cliente"),
     path("clientes/eliminar/<int:id>/", clientes.eliminar_cliente, name="eliminar_cliente"),
+
+    # Urls para proveedores
+    path("proveedores/", proveedores.listar_proveedores, name="listar_proveedores"),
+    path("proveedores/nuevo/", proveedores.crear_proveedor, name="crear_proveedor"),
+    path("proveedores/editar/<int:id>/", proveedores.editar_proveedor, name="editar_proveedor"),
+    path("proveedores/eliminar/<int:id>/", proveedores.eliminar_proveedor, name="eliminar_proveedor"),
 ]
--- app/views/proveedores.py
+++ app/views/proveedores.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django import forms
 from ..models import Proveedor
@@ -25,9 +25,54 @@ class ProveedorForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'https://ejemplo.com'
             }),
-            'direccion': forms.Textarea(attrs={
+            'direccion': forms.TextInput(attrs={
                 'class': 'form-control',
                 'rows': 3,
                 'placeholder': 'Dirección'
             }),
         }
+
+@login_required
+def listar_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, "proveedores/listar_proveedores.html", {
+        "proveedores": proveedores
+    })
+
+@login_required
+def crear_proveedor(request):
+    if request.method == "POST":
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_proveedores")
+    else:
+        form = ProveedorForm()
+
+    return render(request, "proveedores/form_proveedor.html", {
+        "form": form,
+        "titulo": "Agregar proveedor"
+    })
+
+@login_required
+def editar_proveedor(request, id):
+    proveedor = get_object_or_404(Proveedor, id=id)
+
+    if request.method == "POST":
+        form = ProveedorForm(request.POST, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_proveedores")
+    else:
+        form = ProveedorForm(instance=proveedor)
+
+    return render(request, "proveedores/form_proveedor.html", {
+        "form": form,
+        "titulo": "Editar proveedor"
+    })
+
+@login_required
+def eliminar_proveedor(request, id):
+    proveedor = get_object_or_404(Proveedor, id=id)
+    proveedor.delete()
+    return redirect('listar_proveedores')
